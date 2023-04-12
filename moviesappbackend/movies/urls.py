f215214e6f65af5de14f1c612647335728400630
Zain Shahbaz <zainjatt.zj@gmail.com>
2023-04-12 18:27:15 +0500

    vefore iftari...

---
+++ moviesappbackend/movies/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',ListMovies)
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- moviesappbackend/movies/views.py
+++ moviesappbackend/movies/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import *
+from .models import *
 # Create your views here.
+
+
+class ListMovies(generics.ListAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MoviesSerializer
+
+class CreateMovies(generics.CreateAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MoviesSerializer
+
+class DeleteMovies(generics.DestroyAPIView):
+    queryset = Movies.objects.all()
+    serializer_class = MoviesSerializer
+
+
+
+
+
+
+
+
+
+
+
