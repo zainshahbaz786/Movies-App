c00072e0bb64aa6bccbd5ee228f7ca5e913a05d4
Zain Shahbaz <zainjatt.zj@gmail.com>
2023-04-17 16:56:04 +0500

    altu faltu push

--- moviesappbackend/movies/admin.py
+++ moviesappbackend/movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+# Role Based Access Control in Django. This model stores the actual users in the system. It has basic fields like username, password,
+# and email. You can extend this class to add more
+# attributes that your application needs. Django user authentication handles the authentication through session and middle-wares.
