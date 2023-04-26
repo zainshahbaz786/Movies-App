538fe43c910ee8c3e840230514d81bed8226ac60
Zain Shahbaz <zainjatt.zj@gmail.com>
2023-04-26 12:55:04 +0500

    pushing...

--- moviesapp/src/Components/SectionPage.jsx
+++ moviesapp/src/Components/SectionPage.jsx
@@ -4,25 +4,25 @@ import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from
 const features = [
 
     {
-      name: 'Push to deploy',
+      name: 'A place for latest movies',
       description:
         'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
       icon: CloudArrowUpIcon,
     },
     {
-      name: 'SSL certificates',
+      name: 'Secured Web Page',
       description:
         'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
       icon: LockClosedIcon,
     },
     {
-      name: 'Simple queues',
+      name: 'Resueable Free API',
       description:
         'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
       icon: ArrowPathIcon,
     },
     {
-      name: 'Advanced security',
+      name: 'Security Concious platform',
       description:
         'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
       icon: FingerPrintIcon,
@@ -37,13 +37,12 @@ const SectionPage = () => {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">Watch Movies on MoviesHub</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Everything you need to deploy your app
+            Everything you trying to watch is here
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
-            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+           lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. 
           </p>
         </div>
         <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
